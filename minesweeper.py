4d4e6ea4353db88d1a108e6d7994c9c02c4fff5e
Aleksey1120 <alex.sad322@gmail.com>
2024-06-08 18:52:28 +0300

    perf: improve game performance

--- minesweeper.py
+++ minesweeper.py
@@ -1,4 +1,8 @@
+import time
+
 import numpy as np
+
+from numba import njit
 
 
 # 0-8 – mine count;
@@ -34,34 +38,15 @@ class Minesweeper:
         field[mine_idx] = 9
 
         field = field.reshape(self.row_count, self.column_count)
-        for i in range(self.row_count):
-            for j in range(self.column_count):
-                if field[i, j] == 9:
-                    for k in (-1, 0, 1):
-                        for l in (-1, 0, 1):
-                            if 0 <= i + k < self.row_count and 0 <= j + l < self.column_count:
-                                if field[i + k, j + l] != 9:
-                                    field[i + k, j + l] += 1
 
+        update_neighbor_counts(field)
         return field
 
     def get_field(self):
-        visible = np.ones((self.row_count, self.column_count)) * 10
-        visible[self._field_mask.nonzero()] = self._field[self._field_mask.nonzero()]
-        return visible
+        return np.where(self._field_mask, self._field, 10)
 
     def _get_hidden_field(self):
         return self._field
-
-    def recursive_open(self, x, y):
-        if x < 0 or x >= self.row_count or y < 0 or y >= self.column_count:
-            return
-        if self._field_mask[x, y] == 0:
-            self._field_mask[x, y] = 1
-            self.score += 1
-            if self._field[x, y] == 0:
-                for i, j in [(-1, 0), (0, 1), (1, 0), (0, -1)]:
-                    self.recursive_open(x + i, y + j)
 
     def step(self, x, y):
         if self._field_mask[x, y] == 1:
@@ -73,10 +58,40 @@ class Minesweeper:
         if self._field[x, y] == 9:
             return 'lose', self._get_hidden_field()
 
-        self.recursive_open(x, y)
+        open_cells(self._field, self._field_mask, x, y)
         if self.score == self.row_count * self.column_count - self.mine_count:
             return 'win', self._get_hidden_field()
         return 'continue', self.get_field()
+
+
+@njit(fastmath=True)
+def update_neighbor_counts(field):
+    row_count, column_count = field.shape
+    for i in range(row_count):
+        for j in range(column_count):
+            if field[i, j] == 9:
+                for k in (-1, 0, 1):
+                    for l in (-1, 0, 1):
+                        if 0 <= i + k < row_count and 0 <= j + l < column_count:
+                            if field[i + k, j + l] != 9:
+                                field[i + k, j + l] += 1
+
+
+@njit(fastmath=True)
+def open_cells(field, field_mask, x, y):
+    coords = [(x, y)]
+    row_count, column_count = field.shape
+    score = 0
+    for cur_x, cur_y in coords:
+        if cur_x < 0 or cur_x >= row_count or cur_y < 0 or cur_y >= column_count:
+            continue
+        if field_mask[cur_x, cur_y] == 0:
+            field_mask[cur_x, cur_y] = 1
+            score += 1
+            if field[cur_x, cur_y] == 0:
+                for i, j in [(-1, 0), (0, 1), (1, 0), (0, -1)]:
+                    coords.append((cur_x + i, cur_y + j))
+    return score
 
 
 def main():
@@ -92,5 +107,29 @@ def main():
             break
 
 
+def play_game(p_misstep):
+    m = Minesweeper(13, 17, 36, first_step=True)
+    field = m._get_hidden_field()
+    for i in range(13):
+        for j in range(17):
+            if field[i, j] != 9:
+                res, _ = m.step(i, j)
+                if res == 'win':
+                    return
+            elif np.random.random() < p_misstep:
+                res, _ = m.step(i, j)
+                return
+
+
+def speed_test(n_iter, p_misstep):
+    total_time = 0
+    for _ in range(n_iter):
+        start = time.time()
+        play_game(p_misstep)
+        total_time += time.time() - start
+    return total_time / n_iter
+
+
 if __name__ == '__main__':
-    main()
+    # main()
+    print(speed_test(5000, 0.01))
